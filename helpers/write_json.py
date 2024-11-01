d5e17db396e5242a5b1befda744b1f714b7c641b
Matheus Macedo-Lima <math.biomatter@gmail.com>
2024-11-01 13:46:08 -0400

    Minor update to generalize write_json.py

--- helpers/write_json.py
+++ helpers/write_json.py
@@ -2,8 +2,7 @@ from os import sep
 from helpers.NumpyEncoder import NumpyEncoder
 import json
 
-def write_json(cur_unitData, output_path):
-    # # Save as json file
-    json_filename = cur_unitData['Unit'] + '_unitData.json'
-    with open(output_path + sep + 'JSON files' + sep + json_filename, 'w') as cur_json:
-        cur_json.write(json.dumps(cur_unitData, cls=NumpyEncoder, indent=4))
+def write_json(cur_data, output_path, json_filename):
+    # Save as json file
+    with open(output_path + sep + json_filename, 'w') as cur_json:
+        cur_json.write(json.dumps(cur_data, cls=NumpyEncoder, indent=4))
